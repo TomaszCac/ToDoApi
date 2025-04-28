e39de03a119f1bf64dfe24a4bed8ea7ecbe05fe0
Tomasz Cac <tomaszandrzej1234@gmail.com>
2025-04-28 20:09:51 +0200

    Added DBContext

---
+++ ToDoApi/Data/MariaDatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApi.Models;
+
+namespace ToDoApi.Data
+{
+    public class MariaDatabaseContext : DbContext
+    {
+        public MariaDatabaseContext(DbContextOptions<MariaDatabaseContext> options) : base(options) { }
+
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
