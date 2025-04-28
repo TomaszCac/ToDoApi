40e20477f39eab2491c07b368c5d15fad5421378
Tomasz Cac <tomaszandrzej1234@gmail.com>
2025-04-28 19:58:12 +0200

    Added ToDo model, default ToDo controller and removed template files

---
+++ ToDoApi/Models/ToDo.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoApi.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public int CompletePercent { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
